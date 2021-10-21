fe67c18ac6bb0b713a53a37fb777c2358fe103a8
Hythoxe <yann.queric512@laposte.net>
2021-10-21 19:01:19 +0200

    Ajout du fulladder dans le main

--- TME45/src/Cpp_files/Main.cpp
+++ TME45/src/Cpp_files/Main.cpp
@@ -20,21 +20,21 @@ int main ( int argc, char* argv[] )
   new Term( and2, "i0", Term::In  );
   new Term( and2, "i1", Term::In  );
   new Term( and2,  "q", Term::Out );
-  and2->toXml( cout );
+  //and2->toXml( cout );
 
   cout << "\nConstruction du modele <or2>." << endl;
   Cell* or2 = new Cell ( "or2" );
   new Term( or2, "i0", Term::In  );
   new Term( or2, "i1", Term::In  );
   new Term( or2,  "q", Term::Out );
-  or2->toXml( cout );
+  //or2->toXml( cout );
 
   cout << "\nConstruction du modele <xor2>." << endl;
   Cell* xor2 = new Cell ( "xor2" );
   new Term( xor2, "i0", Term::In  );
   new Term( xor2, "i1", Term::In  );
   new Term( xor2,  "q", Term::Out );
-  xor2->toXml( cout );
+  //xor2->toXml( cout );
 
   cout << "\nConstruction du modele <halfadder>." << endl;
   Cell* halfadder = new Cell ( "halfadder" );
@@ -58,7 +58,43 @@ int main ( int argc, char* argv[] )
   ha_and2->connect( "i0", ha_a    );
   ha_and2->connect( "i1", ha_b    );
   ha_and2->connect(  "q", ha_cout );
-  halfadder->toXml( cout );
+  //halfadder->toXml( cout );
+
+  cout << "\nConstruction du modele <fulladder>." << endl;
+  Cell* fulladder = new Cell ( "fulladder" );
+  new Term( fulladder, "a"   , Term::In  );
+  new Term( fulladder, "b"   , Term::In  );
+  new Term( fulladder, "cin" , Term::In  );
+  new Term( fulladder, "sout", Term::Out );
+  new Term( fulladder, "cout", Term::Out );
+  Net*      fu_a    = new Net      ( fulladder, "a"   , Term::External );
+  Net*      fu_b    = new Net      ( fulladder, "b"   , Term::External );
+  Net*      fu_cin  = new Net      ( fulladder, "cin" , Term::External );
+  Net*      fu_sout = new Net      ( fulladder, "sout", Term::External );
+  Net*      fu_cout = new Net      ( fulladder, "cout", Term::External );
+  Net*      fu_sout_1 = new Net    ( fulladder, "sout_1", Term::Internal );
+  Net*      fu_carry_1 = new Net   ( fulladder, "carry_1", Term::Internal );
+  Net*      fu_carry_2 = new Net   ( fulladder, "carry_2", Term::Internal );
+  Instance* fu_ha1  = new Instance ( fulladder, Cell::find("halfadder"), "ha_1" );
+  Instance* fu_ha2  = new Instance ( fulladder, Cell::find("halfadder"), "ha_2" );
+  Instance* fu_or2   = new Instance ( fulladder, Cell::find("or2")     , "or2_1");
+  fulladder->connect( "a"   , fu_a    );
+  fulladder->connect( "b"   , fu_b    );
+  fulladder->connect( "cin" , fu_cin  );
+  fulladder->connect( "sout", fu_sout );
+  fulladder->connect( "cout", fu_cout );
+  fu_ha1->connect( "a", fu_a );
+  fu_ha1->connect( "b", fu_b );
+  fu_ha1->connect( "sout", fu_sout_1 );
+  fu_ha1->connect( "cout", fu_carry_1);
+  fu_ha2->connect( "a", fu_cin );
+  fu_ha2->connect( "b", fu_sout_1 );
+  fu_ha2->connect( "sout", fu_sout );
+  fu_ha2->connect( "cout", fu_carry_2 );
+  fu_or2->connect( "i0", fu_carry_1 );
+  fu_or2->connect( "i1", fu_carry_2 );
+  fu_or2->connect(  "q", fu_cout );
+  fulladder->toXml( cout );
 
   return 0;
 }
